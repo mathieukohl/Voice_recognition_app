676583cac7561c6a5614274dc43cacaa2a3a8123
mathieukohl <mathieu.kohl@students.hevs.ch>
2023-06-15 22:30:31 +0200

    Basic interface transform speech to txt

--- App.tsx
+++ App.tsx
@@ -1,11 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import Voice from '@react-native-voice/voice';
 
 export default function App() {
+
+  let [started, setStarted] = useState(false);
+  let [results, setResults] = useState([]);
+
+  useEffect(() => {
+    Voice.onSpeechError = onSpeechError;
+    Voice.onSpeechResults = onSpeechResults;
+
+    return () => {
+      Voice.destroy().then(Voice.removeAllListeners);
+    } 
+  }, []);
+
+  const startSpeechToText = async () => {
+    await Voice.start("en-US");
+    setStarted(true);
+  }
+
+  const stopSpeechToText = async () => {
+    await Voice.stop();
+    setStarted(false);
+  }
+
+  const onSpeechResults = (result:any) => {
+    setResults(result.value)
+  }
+
+  const onSpeechError = (error:any) => {
+    console.log(error);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      { !started ? <Button title='Start Speech to Text' onPress={startSpeechToText}></Button> : undefined } 
+      { started ? <Button title='Stop Speech to Text' onPress={stopSpeechToText}></Button> : undefined }
+      {results.map((result, index) => <Text key={index}>{result}</Text>)}
       <StatusBar style="auto" />
     </View>
   );
