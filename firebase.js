bc52e42df30dfe410dbd013a646e72093d92cc21
mathieukohl <48278664+mathieukohl@users.noreply.github.com>
2023-07-27 22:05:30 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import { getDatabase } from  '@firebase/database';
 
 const firebaseConfig = {
   // Firebase configuration
-  apiKey: process.env.FIREBASE_apiKey,
-  authDomain: "zupanvoicereco.firebaseapp.com",
-  databaseURL: "https://zupanvoicereco-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "zupanvoicereco",
-  storageBucket: "zupanvoicereco.appspot.com",
-  messagingSenderId: "105878091174",
-  appId: "1:105878091174:web:d420df7358e95a1fd80b67"
+  XXX
 };
 
 const app = initializeApp(firebaseConfig);
